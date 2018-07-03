6bbedab79537490e15238030808c6b634f27dc53
neo7official <40789952+neo7official@users.noreply.github.com>
2018-07-03 15:26:04 +0530

    Add files via upload

---
+++ src/js/about.js
@@ -0,0 +1,4 @@
+import "css/normalize.css";
+import "css/about.scss";
+
+//we can import any CSS or JS file here
---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import "css/normalize.css";
+import "css/index.scss";
+
+//we can import any CSS or JS file here
